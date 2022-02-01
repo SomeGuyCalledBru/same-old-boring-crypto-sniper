c624ee096db81cc9528811eb6f39d5da6fc719b9
SomeGuyCalledBru <98551819+SomeGuyCalledBru@users.noreply.github.com>
2022-02-01 12:16:28 +0300

    part of a kinda

--- http_api.py
+++ http_api.py
@@ -1,22 +1,28 @@
+import logging
+
+
 try:
     import os, sys
     from threading import Thread
     from flask import Flask, request, Response, send_from_directory
     from web3 import Web3
-    from common import log, generate_kwargs
-    import common
     import json
-    from sniperlib import sniper
     import webbrowser
     from time import time, sleep
-    from scrapelib import scraper
+    import common
+    import sniperlib
+    import subprocess
+    import scrapelib
 except Exception as e:
     print(f"CRITICAL: An error occurred during start-up. Message: {e}\n Make sure that your configuration is correct and you have necessary libraries installed.\nIf you're sure everything is right, open an issue over at GitHub")
 DECIMALS_CACHE = {}
+PARAMS_CONVERTED = False
 PORT = 9545
 PRIVATE_INFORMATION = ["privateKey", "telegramApi"]
+log = common.log
 app = Flask(__name__)
-
+sniper = sniperlib.sniper
+scraper = scrapelib.scraper
 @app.route("/")
 def index():
     return send_from_directory('public', 'index.html')
@@ -57,7 +63,8 @@ def is_configured_core():
 
 @app.route("/isTGConfigured")
 def is_tg_configured():
-    return json.dumps(bool(common.config["telegramApi"][0]) and bool(common.config["telegramApi"][1]))
+    log.debug(f'{bool(common.config["telegramApi"][0]) and bool(common.config["telegramApi"][1])}')
+    return 'true' if bool(common.config["telegramApi"][0]) and bool(common.config["telegramApi"][1]) else 'false'
 
 @app.route("/isDeployed")
 def is_deployed():
@@ -65,7 +72,7 @@ def is_deployed():
 
 @app.route("/simulate/<address>")
 def simulate(address):
-    return json.dumps(sniper.simulate(address))
+    return json.dumps(sniper.simulate(address, sniper.AMOUNT_TO_USE))
 
 @app.route("/decs/<address>")
 def decimals(address):
@@ -79,7 +86,6 @@ def decimals(address):
 
 @app.route("/getSimulationsAndTrades")  
 def get_simulations():
-    log.debug("getSimulationsAndTrades")
     return json.dumps({'simulations': sniper.simulations, 'trades': sniper.trades})
 
 @app.route("/getBalance/<address>")
@@ -90,12 +96,37 @@ def get_balance(address):
 def get_scrape_state(_scraper):
     if _scraper == "c":
         return json.dumps(scraper.c)
+    elif _scraper == "t":
+        return json.dumps(scraper.t)
 
-@app.route("/setConfig", methods=["POST"])
-def set_config():
-    with open("config.json", "w") as f:
-        json.dump(request.args.to_dict(), f, indent=4)
-    return json.dumps('""')
+@app.route("/baseGas")
+def base_gas():
+    return json.dumps(sniper.base_gas_price)
+
+@app.route("/getParams")
+def get_params():
+    return json.dumps({
+        "amountUsed": sniper.AMOUNT_TO_USE,
+        "gasParams": sniper.GAS_PARAMS
+    })
+    
+
+@app.route("/setParams", methods=["POST"])
+def set_params():
+    AMOUNT_TO_USE = int(round(float(request.args.get('amount_used'))))
+    GAS_PARAMS = json.loads(request.args.get('gas_params'))
+    GAS_PARAMS[0] = int(round(float(GAS_PARAMS[0])))
+    if None in GAS_PARAMS or AMOUNT_TO_USE == None:
+        return Response('"Fields cannot be empty"', status=400)
+    if GAS_PARAMS[1] < 50000:
+        return Response('"Gas limit cannot be below 50K"', status=400)
+    if GAS_PARAMS[0] < sniper.base_gas_price and not GAS_PARAMS[2]:
+        return Response('"Gas price cannot be below base without adaptive gas enabled"', status=400)
+    if AMOUNT_TO_USE == 0:
+        return Response('"Amount cannot be zero"', status=400)
+    sniper.set_params(AMOUNT_TO_USE, GAS_PARAMS)
+    return '""'
+
 
 @app.route("/deploy", methods=["POST"])
 def deploy():
@@ -121,6 +152,8 @@ def stop_simulating(address):
 def toggle_scrape(_scraper):
     if _scraper == "c":
         scraper.toggle_C()
+    elif _scraper == "t":
+        scraper.toggle_T()
     return '""'
 
 @app.route("/buy", methods=["POST"])
@@ -129,11 +162,10 @@ def buy():
     Buys token with provided parameters.
     Parameters:
     address[REQUIRED]: Address for token. 
-    amount_in[Optional]: If specified, overrides spec in configuration.
     """
     if not request.args.get("address"):
         return '"Invalid request: required params missing"'
-    return json.dumps(str(sniper.swap(Web3.toChecksumAddress(request.args.get("address").replace(" ", "")), "buy", amount_in=Web3.toWei(request.args.get("amount_in", type=float), 'ether') if request.args.get("amount_in", type=float) != None else None)))
+    return json.dumps(str(sniper.swap(Web3.toChecksumAddress(request.args.get("address").replace(" ", "")), "buy", Web3.toWei(sniper.AMOUNT_TO_USE, 'ether'), sniper.GAS_PARAMS)))
 
 @app.route("/sell", methods=["POST"])
 def sell():
@@ -144,7 +176,7 @@ def sell():
     """
     if not request.args.get("address"):
         return '"Invalid request: required params missing"'
-    return json.dumps(str(sniper.swap(Web3.toChecksumAddress(request.args.get("address").replace(" ", "")),  side="sell", amount_in=request.args.get("amount_in", type=float))))
+    return json.dumps(str(sniper.swap(Web3.toChecksumAddress(request.args.get("address").replace(" ", "")), "sell", request.args.get("amount_in", type=float), sniper.GAS_PARAMS)))
 
 @app.route("/pnl", methods=["GET"])
 def pnl():
@@ -156,17 +188,47 @@ def ping():
     Does nothing but serve as a ping endpoint.
     """
     if not common.w3.isConnected():
-        return Response('""', status=418)
+        return Response('""', status=503)
     return Response('""', status=200)
 
 @app.route("/kill", methods=["POST"])
 def kill():
     Thread(target=_kill).start()
+    subprocess.Popen(["python", "main.py", "1"], close_fds=True)
+
+@app.route("/set_ND", methods=["POST"])
+def set_network_and_dex():
+    if not network_dex_pair_valid(request.args.get("network"), request.args.get("dex")):
+        return Response('"Invalid network-dex pair"', status=400)
+    common.config["network"] = request.args.get("network")
+    common.config["dex"] = request.args.get("dex")
+    common.set_config()
+    return '""'
+
+@app.errorhandler(404)
+def pnf(error):
+    return f'Literally nothing to see here!', 404
+
+@app.errorhandler(500)
+def ise(error):
+    return Response(f'"Whoops, you got us bad. Something must not have happened but it happened anyway. Error: {error}"', status=500)
 
 def _kill():
-    sleep(0.5)
+    sleep(0.1)
+    log.debug("Rekting")
     os._exit(1)
 
-def init():
-    webbrowser.open(f"http://127.0.0.1:{PORT}")
+def network_dex_pair_valid(network, dex):
+    try:
+        common.network_presets["dexes"][network][dex]["name"]
+        return True
+    except Exception:
+        return False
+
+def init(is_reset):
+    # Let port free on restart
+    if is_reset:
+        sleep(1)
+    else:
+        webbrowser.open("http://127.0.0.1:9545")
     app.run(host="127.0.0.1", port=PORT)
