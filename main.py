fd98a17ab2dd968b8614704de4f835d65fd15740
SomeGuyCalledBru <98551819+SomeGuyCalledBru@users.noreply.github.com>
2022-02-01 12:18:07 +0300

    add a restart handler

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import http_api
+import sys
+try:
+    if sys.argv[1] != '1':
+        is_reset = "dont_start"
+    else:
+        is_reset = True
+except IndexError:
+    is_reset = False
 if __name__ == "__main__":
-    http_api.init()
+    if is_reset != "dont_start":
+        http_api.init(is_reset=is_reset)
+    else:
+        http_api.log.setLevel(50)
